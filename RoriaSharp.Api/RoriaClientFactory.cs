fdb4e5d955189d1147a71bd18aa7a564aa842792
SaltWood_233 <105980161+SALTWOOD@users.noreply.github.com>
2026-03-22 16:45:09 +0900

    feat(api): add `RoriaClientFactory` for ApiClient creation

---
+++ RoriaSharp.Api/RoriaClientFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+using Microsoft.Kiota.Abstractions.Authentication;
+using RoriaSharp.Api.Generated;
+
+namespace RoriaSharp.Api;
+
+public static class RoriaClientFactory
+{
+    public static ApiClient Create(string baseUrl, HttpClient? httpClient = null)
+    {
+        // TODO: Pass token here
+        var authProvider = new AnonymousAuthenticationProvider();
+        
+        // Initialize adapter
+        var adapter = new HttpClientRequestAdapter(authProvider, httpClient: httpClient)
+        {
+            BaseUrl = baseUrl
+        };
+
+        return new ApiClient(adapter);
+    }
+}
