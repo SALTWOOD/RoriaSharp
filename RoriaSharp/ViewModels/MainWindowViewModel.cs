18b6bc1d0e47bb53d365c6bc0a1b8755ccab0747
SaltWood_233 <105980161+SALTWOOD@users.noreply.github.com>
2026-03-22 15:54:10 +0900

    initial commit

---
+++ RoriaSharp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace RoriaSharp.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ RoriaSharp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace RoriaSharp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
